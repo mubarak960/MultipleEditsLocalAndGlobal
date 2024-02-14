9f1d0d5469bc77f99276b402693709c0fc285b0a
mubarak.shaik <mbshaik960@gmail.com>
2024-02-14 12:03:40 +0530

    second step added in processing.

--- src/LoanMovement/Processing.java
+++ src/LoanMovement/Processing.java
@@ -5,6 +5,7 @@ public class Processing {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is in processing");
+		System.out.println("This will move to underwriting");
 
 	}
 
